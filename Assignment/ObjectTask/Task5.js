9a10fd0931abf97ec97bdcee60fa436b50e6ea39
Sakshi Kamble <sakshikamble0609@gmail.com>
2026-03-17 21:53:58 +0530

    Object Task done

--- Assignment/ObjectTask/Task5.js
+++ Assignment/ObjectTask/Task5.js
@@ -1,5 +1,22 @@
-// Create Calculator object
-let Calculator={
-       
-    add
+
+const person1={
+    name : "Anushka",
+    age:23,  
+};
+
+const person2={
+    name : "Shruti",
+    age:23,
+};
+
+if(person1.name===person2.name){
+    console.log("Names are same")
+}else{
+    console.log("Names are different")
+}
+
+if(person1.age===person2.age){
+    console.log("age are same")
+}else{
+    console.log("age are different")
 }
